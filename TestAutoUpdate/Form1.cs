976e489a6d30ea7a87746acd03a13adf0588ff1f
Harshada Deshpande <harshada@vaspsolutions.com>
2019-07-17 11:04:55 +0530

    Add project files.

---
+++ TestAutoUpdate/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoUpdaterDotNET;
+
+namespace TestAutoUpdate
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			AutoUpdater.Start(@"D:\Business\TestAutoUpdate\TestAutoUpdate\Updater\AutoUpdaterTest.xml");
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			label1.Text = "This is version " + this.ProductVersion;
+		}
+	}
+}
